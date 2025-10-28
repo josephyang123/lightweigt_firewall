2ce01aa254598469e6986f225abb06facf95b9f6
josephyang123 <wcies5510@gmail.com>
2025-10-28 08:43:31 -0700

    removed headings

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <cstring>      // For memset
+#include <sys/socket.h> // For socket functions
+#include <netinet/ip.h> // For iphdr struct
+#include <arpa/inet.h>  // For inet_ntoa
+#include <unistd.h>     // For close()
 
 int main() {
     std::cout << "MiniWall Firewall Started!" << std::endl;
